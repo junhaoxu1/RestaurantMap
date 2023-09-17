c360cac637eabefec4db04083c35a80f0bcee334
Patrik He <patrikswgit@gmail.com>
2023-09-17 21:16:32 +0200

    added BrowserRouter to the app, to be able to use route path to access Home and Map pages
    also added bootstrap container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Container } from 'react-bootstrap'
 import './assets/scss/App.scss'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MapPage from './pages/MapPage'
 
 const App = () => {
 
 	return (
 		<div id='App'>
-			<h1>Maps</h1>
+			<Container>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/food-map" element={<MapPage />} />
+				</Routes>
+			</Container>
 		</div>
 	)
 }
