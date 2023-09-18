561785a8e6f853e5d81c8dac2bf503f20a52b6d6
Andreas X <97689187+junhaoxu1@users.noreply.github.com>
2023-09-18 13:07:25 +0200

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './assets/scss/App.scss'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MapPage from './pages/MapPage'
+import Navigation from './components/Navigation'
 
 const App = () => {
 
 	return (
 		<div id='App'>
+			<Navigation />
 			<Container>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { NavLink, Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <Navbar>
+        <Container>
+            <Navbar.Brand as={Link} to ="/">
+                Restaurants
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link as={NavLink} end to="/">
+                        Login
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} end to="/">
+                        Sign Up
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
