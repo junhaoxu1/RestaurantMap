20ff3b99176a12bca780fbe780ab1bb25977fe99
Patrik He <patrikswgit@gmail.com>
2023-09-18 21:59:46 +0200

    Fixed the structure for signuppage.
    Changed center lat and lng to
    medieinstitutet Malmö inside map file.
    
    also added removing google maps buttons inside MapOptions.
    even added get current position by clicking on the icon and added SearchPlaceComp.
    
    Created searchPlaceComponent
    with form input button for searching
    the place.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,22 +1,78 @@
-import { useMemo } from "react";
-import { GoogleMap } from "@react-google-maps/api";
+import { useMemo } from "react"
+import { GoogleMap } from "@react-google-maps/api"
+import { Button } from "react-bootstrap"
+import SearchPlaceComp from "./SearchPlaceComp"
+import { useSearchParams } from "react-router-dom"
+import React from "react"
+
+type MapOptions = google.maps.MapOptions
 
 const Map = () => {
-	// it remember to locationen by setting latitude and longitude
-	const center = useMemo(() => ({lat: 55, lng: 13}), [])
+    // it remember to locationen by setting latitude and longitude
+    const center = useMemo(() => ({ lat: 55.6053, lng: 13.0041 }), [])
 
-	return (
-		<div className="cBox">
-			<div className="map">
-				<GoogleMap
-					zoom={10}
-					center={center}
-					mapContainerClassName="map-container"
-				>
-				</GoogleMap>
-			</div>
-		</div>
-	)
+    const [_gCurrentPos, setGCurrentPos] = React.useState<google.maps.LatLngLiteral>({} as google.maps.LatLngLiteral)
+    const [_disabled, setDisabled] = React.useState(false)
+
+    //removing googleMaps zoom in&out button and other button inside MapOptions.
+    const options = useMemo<MapOptions>(
+        () => ({
+            disableDefaultUI: true,
+            clickableIcons: false,
+        }),
+        []
+    )
+
+    const [_searchParams, setSearchParams] = useSearchParams({
+        query: "",
+    })
+
+    const handlePlaceForm = async (query: string) => {
+        //set input value as query in searchParams and while getting result
+        setSearchParams({ query })
+    }
+
+    // mapReference can either be reference to google maps or null
+    const mapReference = React.useRef<google.maps.Map | null>(null)
+
+    // getting current position on the google maps by updating state variable getCurrentPos while clicking the icon button
+    const gCurrentPosition = (position: google.maps.LatLngLiteral) => {
+        if (mapReference.current) {
+            mapReference.current.panTo({ lat: position.lat, lng: position.lng })
+            mapReference.current.setZoom(12)
+            setGCurrentPos(position)
+        }
+    }
+
+    // return updated reference with current map for interacting with google maps
+    const onMapLoadInstance = (map: google.maps.Map): void => {
+        mapReference.current = map
+    }
+
+    return (
+        <div className="cBox">
+            <Button
+                onClick={() => {
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        // Click the button when geolocation has finished loaded
+                        setDisabled(false)
+                        // get the callback function for your position
+                        gCurrentPosition({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        })
+                    })
+                }}
+                style={{ color: "grey", background: "black", position: "absolute", zIndex: "2", left: "19em" }}
+            >
+                <i className="fa-solid fa-location-crosshairs"></i>
+            </Button>
+            <div className="map">
+                <GoogleMap zoom={12} center={center} mapContainerClassName="map-container" options={options} onLoad={onMapLoadInstance}></GoogleMap>
+                <SearchPlaceComp onSearchPlace={handlePlaceForm} />
+            </div>
+        </div>
+    )
 }
 
 export default Map
