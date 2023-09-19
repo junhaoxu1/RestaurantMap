9f3a335fbb0a7189680033b1e8a0d6558c2fd65a
sb <dev.simon.bengtsson@gmail.com>
2023-09-19 10:56:12 +0200

    styled, and set validation for form data.

--- src/components/AddNewRestaurantForm.tsx
+++ src/components/AddNewRestaurantForm.tsx
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react'
-import Form from 'react-bootstrap/Form'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { RestaurantFormData } from '../types/restaurants.types'
-import Button from 'react-bootstrap/Button'
+import React, { useEffect } from "react"
+import Form from "react-bootstrap/Form"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { RestaurantFormData } from "../types/restaurants.types"
+import Button from "react-bootstrap/Button"
 
 type Props = {
     onAddRestaurant: (data: RestaurantFormData) => Promise<void>
 }
 
 const AddNewRestaurantForm: React.FC<Props> = ({ onAddRestaurant }) => {
-    const { handleSubmit, register, formState: { errors, isSubmitSuccessful }, reset } = useForm<RestaurantFormData>()
-    
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitSuccessful },
+        reset,
+    } = useForm<RestaurantFormData>()
+
     const onFormSubmit: SubmitHandler<RestaurantFormData> = async (data: RestaurantFormData) => {
         // passing `data` to parent
         await onAddRestaurant({
             ...data,
-        }) 
+        })
     }
 
     // reset form upon mount/reset and successful submit
@@ -23,188 +28,157 @@ const AddNewRestaurantForm: React.FC<Props> = ({ onAddRestaurant }) => {
         reset()
     }, [isSubmitSuccessful, reset])
     return (
-        <Form onSubmit={handleSubmit(onFormSubmit)}>
-            
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    Restaurant's name
-                </Form.Label>
+        <Form onSubmit={handleSubmit(onFormSubmit)} className="form-container">
+            <p className="h2">Recommend a restaurant</p>
+            <Form.Group className="mt-3">
+                <Form.Label>Restaurant's name</Form.Label>
                 <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="The restaurant's name"
-                        {...register('name', {
-                            required: "Please enter the name of the restaurant...",
-                            minLength: {
-                                value: 3,
-                                message: "Restaurants name has to be at least 3 characters..."
-                            },
-                        })}
-                    />
+                    type="text"
+                    className="form-control"
+                    aria-label="The restaurant's name"
+                    {...register("name", {
+                        required: "Please enter the name of the restaurant...",
+                        minLength: {
+                            value: 3,
+                            message: "Restaurants name has to be at least 3 characters...",
+                        },
+                    })}
+                />
+                {errors.name && <p className="input-error">{errors.name.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    Address
-                </Form.Label>
+            <Form.Group className="mt-3">
+                <Form.Label>Address</Form.Label>
                 <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="The restaurant's address"
-                        {...register('address', {
-                            required: "Please enter the address to the restaurant...",
-                            minLength: {
-                                value: 5,
-                                message: "Address has to be at least 5 characters long"
-                            },
-                        })}
-                    />
+                    type="text"
+                    className="form-control"
+                    aria-label="The restaurant's address"
+                    {...register("address", {
+                        required: "Please enter the address to the restaurant...",
+                        minLength: {
+                            value: 5,
+                            message: "Address has to be at least 5 characters long",
+                        },
+                    })}
+                />
+                {errors.address && <p className="input-error">{errors.address.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    City
-                </Form.Label>
+            <Form.Group className="mt-3">
+                <Form.Label>City</Form.Label>
                 <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="City where restaurant is located"
-                        {...register('city', {
-                            required: "Please enter the city where the restaurant is located...",
-                            minLength: {
-                                value: 3,
-                                message: "City has to be at least 3 characters long"
-                            },
-                        })}
-                    />
+                    type="text"
+                    className="form-control"
+                    aria-label="City where restaurant is located"
+                    {...register("city", {
+                        required: "Please enter the city where the restaurant is located...",
+                        minLength: {
+                            value: 3,
+                            message: "City has to be at least 3 characters long",
+                        },
+                    })}
+                />
+                {errors.city && <p className="input-error">{errors.city.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    Description
-                </Form.Label>
+            <Form.Group className="mt-3">
+                <Form.Label>Description</Form.Label>
                 <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Descriptiton of the restaurant"
-                        {...register('description', {
-                            required: "Please enter a short description for the restaurant...",
-                            minLength: {
-                                value: 5,
-                                message: "Description has to be at least 5 characters long"
-                            },
-                        })}
-                    />
+                    as="textarea"
+                    rows={3}
+                    className="form-control"
+                    aria-label="Descriptiton of the restaurant"
+                    {...register("description", {
+                        required: "Please enter a short description for the restaurant...",
+                        minLength: {
+                            value: 5,
+                            message: "Description has to be at least 5 characters long",
+                        },
+                        maxLength: {
+                            value: 50,
+                            message: "Description has to be less than 50 characters",
+                        },
+                    })}
+                />
+                {errors.description && <p className="input-error">{errors.description.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    Categories
-                </Form.Label>
-                <Form.Select 
-                    aria-label='Select categories' 
-                    required 
+            <Form.Group className="mt-3">
+                <Form.Label>Categories</Form.Label>
+                <Form.Select
+                    aria-label="Select categories"
+                    required
                     {...register("category", {
-                        required: "Please select an option"
+                        required: "Please select an option",
                     })}
                 >
-                    <option>Please select category</option>
+                    <option value="" selected hidden>
+                        Select an option
+                    </option>
                     <option value="café">Café</option>
                     <option value="restaurant">Restaurant</option>
                     <option value="fast_food">Fast Food</option>
                     <option value="kioskGrill">Kiosk/Grill</option>
                     <option value="foodtruck">Foodtruck</option>
                 </Form.Select>
+                {errors.category && <p className="input-error">{errors.category.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
-                <Form.Label>
-                    Supply
-                </Form.Label>
-                <Form.Select 
-                    aria-label='Select Supply' 
+            <Form.Group className="mt-3">
+                <Form.Label>Supply</Form.Label>
+                <Form.Select
+                    aria-label="Select Supply"
                     {...register("supply", {
-                        required: "Please select an option"
+                        required: "Please select an option",
                     })}
                 >
-                    <option>Please select supply</option>
+                    <option value="" selected hidden>
+                        Select an option
+                    </option>
                     <option value="lunch">Lunch</option>
                     <option value="afterWork">After Work</option>
                     <option value="dinner">Dinner / Á la carte</option>
                 </Form.Select>
+                {errors.supply && <p className="input-error">{errors.supply.message ?? "Invalid value"}</p>}
             </Form.Group>
 
-            <Form.Group className='mt-3'>
+            <Form.Group className="mt-3">
                 <Form.Label>
-                    Email <span className='text-muted'>(optional)</span>
+                    Email <span className="text-muted">(optional)</span>
                 </Form.Label>
-                <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Email to the restaurant"
-                        {...register("email")}
-                />
+                <Form.Control type="text" className="form-control" aria-label="Email to the restaurant" {...register("email")} />
             </Form.Group>
-            <Form.Group className='mt-3'>
+            <Form.Group className="mt-3">
                 <Form.Label>
-                    Phone <span className='text-muted'>(optional)</span>
+                    Phone <span className="text-muted">(optional)</span>
                 </Form.Label>
-                <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Phone to the restaurant"
-                        {...register("phone")}
-                />
+                <Form.Control type="text" className="form-control" aria-label="Phone to the restaurant" {...register("phone")} />
             </Form.Group>
 
-            <Form.Group className='mt-3'>
+            <Form.Group className="mt-3">
                 <Form.Label>
-                    Website <span className='text-muted'>(optional)</span>
+                    Website <span className="text-muted">(optional)</span>
                 </Form.Label>
-                <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Webpage address"
-                        {...register("webpage")}
-                />
+                <Form.Control type="text" className="form-control" aria-label="Webpage address" {...register("webpage")} />
             </Form.Group>
 
-            <Form.Group className='mt-3'>
+            <Form.Group className="mt-3">
                 <Form.Label>
-                    Facebook <span className='text-muted'>(optional)</span>
+                    Facebook <span className="text-muted">(optional)</span>
                 </Form.Label>
-                <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Facebook"
-                        {...register("facebook")}
-                />
+                <Form.Control type="text" className="form-control" aria-label="Facebook" {...register("facebook")} />
             </Form.Group>
 
-            <Form.Group className='mt-3'>
+            <Form.Group className="mt-3">
                 <Form.Label>
-                    Instagram <span className='text-muted'>(optional)</span>
+                    Instagram <span className="text-muted">(optional)</span>
                 </Form.Label>
-                <Form.Control
-                        type="text"
-                        className="form-control"
-                        aria-label="Instagram"
-                        {...register("instagram")}
-                />
+                <Form.Control type="text" className="form-control" aria-label="Instagram" {...register("instagram")} />
             </Form.Group>
 
-            <Button 
-                type='submit'
-                className='mt-3'
-            >Send Recommendation</Button>
-
-            {/* Display error message if form fails to submit */}
-            {errors.name && <p>{errors.name.message ?? "Invalid value"}</p>}
-            {errors.address && <p>{errors.address.message ?? "Invalid value"}</p>}
-            {errors.city && <p>{errors.city.message ?? "Invalid value"}</p>}
-            {errors.description && <p>{errors.description.message ?? "Invalid value"}</p>}
-            {errors.category && <p>{errors.category.message ?? "Invalid value"}</p>}
-            {errors.supply && <p>{errors.supply.message ?? "Invalid value"}</p>}
+            <Button type="submit" className="mt-3 w-50">
+                Send Recommendation
+            </Button>
         </Form>
     )
 }
--- src/pages/RestaurantsPage.tsx
+++ src/pages/RestaurantsPage.tsx
@@ -1,39 +1,35 @@
-import { Link } from 'react-router-dom'
-import useGetCollection from '../hooks/useGetCollection'
-import { newRestaurantCol, restaurantsCol } from '../services/firebase'
-import { Restaurant, RestaurantFormData } from '../types/restaurants.types'
-import ListGroup from 'react-bootstrap/ListGroup'
-import AddNewRestaurantForm from '../components/AddNewRestaurantForm'
-import { doc, setDoc } from 'firebase/firestore'
-import { toast } from 'react-toastify'
-import { getGeocode } from '../services/Geocode'
-
+import { Link } from "react-router-dom"
+import useGetCollection from "../hooks/useGetCollection"
+import { newRestaurantCol, restaurantsCol } from "../services/firebase"
+import { Restaurant, RestaurantFormData } from "../types/restaurants.types"
+import ListGroup from "react-bootstrap/ListGroup"
+import AddNewRestaurantForm from "../components/AddNewRestaurantForm"
+import { doc, setDoc } from "firebase/firestore"
+import { toast } from "react-toastify"
+import { getGeocode } from "../services/Geocode"
 
 const RestaurantsPage = () => {
     const { data: restaurants, error, loading } = useGetCollection<Restaurant>(restaurantsCol)
 
     // add a new restaurant to db
     const addNewRestaurant = async (data: RestaurantFormData) => {
-
         try {
             // get geo code for the submitted address
             const res = await getGeocode(data.address)
 
             // add new document to `restaurants` collection in db
             const docRef = doc(newRestaurantCol)
-    
+
             // set content of the document
             await setDoc(docRef, {
                 ...data,
                 geolocation: {
                     lat: res.lat,
-                    lng: res.lng
-                }   
+                    lng: res.lng,
+                },
             })
-
         } catch (err: any) {
             toast.error(err.message)
-
         }
 
         toast.success("Form successfully sent. Thank you!")
@@ -47,24 +43,19 @@ const RestaurantsPage = () => {
         <div>
             {loading && <p>Loading restaurants...</p>}
             <ListGroup>
-                {restaurants.map(restaurant => (
-                    <ListGroup.Item
-                        key={restaurant.name}
-                        action
-                        as={Link}
-                        to={`/restaurants/${restaurant._id}`}
-                    >
+                {restaurants.map((restaurant) => (
+                    <ListGroup.Item key={restaurant.name} action as={Link} to={`/restaurants/${restaurant._id}`}>
                         <p>{restaurant.name}</p>
                     </ListGroup.Item>
                 ))}
             </ListGroup>
 
-            <AddNewRestaurantForm 
+            {/* <AddNewRestaurantForm 
                 onAddRestaurant={addNewRestaurant}
-            />
+            /> */}
 
             {error && <p>Something went wrong: {error}</p>}
-        </div>  
+        </div>
     )
 }
 
