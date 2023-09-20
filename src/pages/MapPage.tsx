ba00f7045a2c8edba00fe25ca11d388a4d28f964
Patrik He <patrikswgit@gmail.com>
2023-09-20 04:07:34 +0200

    imported useGetCollection hook, restaurant types and resturantsCol firebase to be able to fetch data and render out into the maps.

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,27 +1,38 @@
-import { useLoadScript } from "@react-google-maps/api";
-import Map from "../components/Map";
+import { useLoadScript } from "@react-google-maps/api"
+import Map from "../components/Map"
+import useGetCollection from "../hooks/useGetCollection"
+import { Restaurant } from "../types/restaurants.types"
+import { restaurantsCol } from "../services/firebase"
 const MYMAPSKEY: string = import.meta.env.VITE_APP_GOOGLE_KEY
 
 const MapPage = () => {
-	// Loading Google Maps by using useLoadScript hook and libary places,
-	// it also used to determine when API is fully loaded.
-	const { isLoaded } = useLoadScript({
-		googleMapsApiKey: MYMAPSKEY,
-		libraries: ["places"],
-	})
+    const { data: restaurants, error, loading } = useGetCollection<Restaurant>(restaurantsCol)
 
-	// if it flase then display loading otherwise stop display
-	if (!isLoaded) {
-		return <div>loading ... </div>
-	}
+    // Loading Google Maps by using useLoadScript hook and libary places,
+    // it also used to determine when API is fully loaded.
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: MYMAPSKEY,
+        libraries: ["places"],
+    })
 
-	return (
-		<>
-			<section className="map-page">
-				<Map />
-			</section>
-		</>
-	)
+    // if it flase then display loading otherwise stop display
+    if (!isLoaded) {
+        return <div>loading ... </div>
+    }
+
+    if (!restaurants || loading) {
+        return <p>Loading</p>
+    }
+
+    return (
+        <>
+            <section className="map-page">
+                <Map restaurants={restaurants} />
+            </section>
+
+            {error && <p>Something went wrong: {error}</p>}
+        </>
+    )
 }
 
 export default MapPage
