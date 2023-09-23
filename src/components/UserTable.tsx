1e473594cf173a8b31a8f21c3779951fc96a1f8f
Patrik He <patrikswgit@gmail.com>
2023-09-23 18:38:28 +0200

    added profile photo to display also added name.
    still in progress with fixing the input that can change the name
    and save it in firebase

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/esm/Form";
 import { useTable, Column } from "react-table";
 import useAuth from "../hooks/useAuth";
 import { UpdateUserFormData } from "../types/User.types";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { FirebaseError } from "firebase/app";
+import { doc, getDocs, updateDoc } from "firebase/firestore";
+import { usersCol } from "../services/firebase";
+import { Button, Image } from "react-bootstrap";
 
 interface RowData {
   documentId: string;
   email: string;
   admin: boolean;
   name: string;
+  photoFile: string;
 }
 
 interface AdminTableProps {
   data: RowData[];
   onAdminStatusToggle: (documentId: string, newAdminStatus: boolean) => void;
+//   onAdminNameToggle: (documentId: String, newName: string) => void;
 }
 
 //Lägg till kolumner inne i table
 const AdminTable: React.FC<AdminTableProps> = ({ data, onAdminStatusToggle }) => {
-	const { currentUser } = useAuth()
+	const [error, setError] = useState<string | null>(null)
+	const [loading, setLoading] = useState(false)
+	const { currentUser, reloadUser, setDisplayName, userPhotoUrl } = useAuth()
 	const {
+		handleSubmit,
 		register,
 		formState: { errors },
 	} = useForm<UpdateUserFormData>({
@@ -30,6 +39,48 @@ const AdminTable: React.FC<AdminTableProps> = ({ data, onAdminStatusToggle }) =>
 		},
 	})
 
+	if (!currentUser) {
+		return <p>Failed</p>
+	}
+
+	const onAdminUpdateName: SubmitHandler<UpdateUserFormData> = async (data) => {
+		setError(null)
+
+		try {
+			setLoading(true)
+
+			const querySnapshot = await getDocs(usersCol)
+			querySnapshot.forEach(async (info) => {
+				const userData = info.data()
+				if (userData.uid === currentUser.uid) {
+					const updatedData = {
+						...userData,
+						name: data.name,
+					}
+
+					const userDocRef = doc(usersCol, info.id)
+					await updateDoc(userDocRef, updatedData)
+				}
+			})
+
+			if (data.name !== (currentUser.displayName ?? "")) {
+				await setDisplayName(data.name)
+			}
+
+			await reloadUser()
+
+			setLoading(false)
+
+
+		} catch (error) {
+			if (error instanceof FirebaseError) {
+				setError(error.message)
+			} else {
+				setError("Failed")
+			}
+		}
+	}
+
   const columns: Column<RowData>[] = React.useMemo(
     () => [
       {
@@ -37,17 +88,28 @@ const AdminTable: React.FC<AdminTableProps> = ({ data, onAdminStatusToggle }) =>
         accessor: "email",
       },
 	  {
-		Header: "Name",
+		Header: "Name & ProfileImg",
 		accessor: "name",
 		Cell: ({ row }) => (
 			<Form>
-			<Form.Label>{row.original.name}</Form.Label>
-				<Form.Control
-					style={{width: "50%"}}
-					placeholder={row.original.name ? `${row.original.name}`: "null"}
-					{...register('name')}
-				/>
-				{errors.name && <p className="invalid">{errors.name.message ?? "Invalid value"}</p>}
+				<Form.Group>
+				<div className="profile-photo-wrapper text-center my-2">
+					<div className="d-flex">
+						<Image src={row.original.photoFile || "https://via.placeholder.com/225"} fluid rounded className="img-square w-50" />
+					</div>
+				</div>
+				<Form.Label>{row.original.name ? `${row.original.name}` : "(no name)"}</Form.Label>
+					<Form.Control
+						style={{width: "50%"}}
+						placeholder={row.original.name ? `${row.original.name}`: "null"}
+						defaultValue={row.original.name || ""}
+						// {...register('name')}
+					/>
+					{errors.name && <p className="invalid">{errors.name.message ?? "Invalid value"}</p>}
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					{row.original.name || "null"}
+				</Button>
 			</Form>
         ),
 	  },
