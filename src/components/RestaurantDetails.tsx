86ecd9148796037938ce0af0ca0b34718c64d662
Andreas X <97689187+junhaoxu1@users.noreply.github.com>
2023-09-26 16:54:44 +0200

    Request new photo + Render photo from admin

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -6,71 +6,82 @@ import { FaFacebook, FaInstagram } from "react-icons/fa"
 import { Container } from "react-bootstrap"
 
 type Props = {
-    restaurant: Restaurant
+	restaurant: Restaurant
 }
 
 const RestaurantDetails: React.FC<Props> = ({ restaurant }) => {
-    return (
-        <>
-            <div className="banner d-flex">
-                <Image src={restaurant.cover_photo} style={{ width: "100%", objectFit: "cover" }} />
-                <p className="h2 w-100 text-center my-auto py-3">{restaurant.name}</p>
-            </div>
+	return (
+		<>
+			<div className="banner d-flex">
+				<Image src={restaurant.cover_photo} style={{ width: "100%", objectFit: "cover" }} />
+				<p className="h2 w-100 text-center my-auto py-3">{restaurant.name}</p>
+			</div>
 
-            <Container className="restaurant-content mx-auto">
-                <div className="card-wrapper mb-5">
-                    <Card className="mt-3 shadow">
-                        <Card.Body>
-                            <Card.Title className="card-heading">Information</Card.Title>
-                            <Card.Text className="card-text mt-3">
-                                <span className="details-subheading">About the restaurant:</span> <br /> {restaurant.description}
-                            </Card.Text>
-                            <Card.Text className="card-text mt-3">
-                                <span className="details-subheading">Tags:</span> <br /> {restaurant.supply}, {restaurant.category}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+			<Container className="restaurant-content mx-auto">
+				<div className="card-wrapper mb-5">
+					<Card className="mt-3 shadow">
+						<Card.Body>
+							<Card.Title className="card-heading">Information</Card.Title>
+							<Card.Text className="card-text mt-3">
+								<span className="details-subheading">About the restaurant:</span> <br /> {restaurant.description}
+							</Card.Text>
+							<Card.Text className="card-text mt-3">
+								<span className="details-subheading">Tags:</span> <br /> {restaurant.supply}, {restaurant.category}
+							</Card.Text>
+							<Card className="card-text mt-3">
+								<div className="image-gallery">
+									{restaurant.user_photos?.map((photo, index) => (
+										<Image
+											key={index}
+											src={photo.photo}
+											style={{ width: "100%", objectFit: "cover" }}
+										/>
+									))}
+								</div>
+							</Card>
+						</Card.Body>
+					</Card>
 
-                    <Card className="mt-3 shadow">
-                        <Card.Body>
-                            <Card.Title className="card-heading">Contact</Card.Title>
-                            <Card.Text className="card-text mt-3">
-                                <span className="details-subheading">Location:</span> <br /> {restaurant.address}, {restaurant.city}
-                            </Card.Text>
-                            <Card.Text className="card-text mt-3">
-                                <span className="details-subheading">Phone:</span> <br /> {restaurant.phone || "N/A"}
-                            </Card.Text>
-                            <Card.Text className="card-text mt-3">
-                                <span className="details-subheading">Email:</span> <br /> {restaurant.email || "N/A"}
-                            </Card.Text>
-                            <div className="card-text mt-3">
-                                <span className="details-subheading">Website:</span> <br />{" "}
-                                <a href={`https://${restaurant.webpage}`} target="_blank">
-                                    {restaurant.webpage || "N/A"}
-                                </a>
-                            </div>
-                            <div className="card-text mt-3">
-                                <span className="details-subheading">Social Media:</span> <br />
-                                <span className="social-media-container d-flex gap-2">
-                                    {!restaurant.email && !restaurant.facebook && !restaurant.instagram && <p>Data missing</p>}
-                                    {restaurant.facebook !== "" && (
-                                        <a href={restaurant.facebook} className="fa-icon">
-                                            <FaFacebook />
-                                        </a>
-                                    )}
-                                    {restaurant.instagram !== "" && (
-                                        <a href={restaurant.instagram} className="fa-icon">
-                                            <FaInstagram />
-                                        </a>
-                                    )}
-                                </span>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </Container>
-        </>
-    )
+					<Card className="mt-3 shadow">
+						<Card.Body>
+							<Card.Title className="card-heading">Contact</Card.Title>
+							<Card.Text className="card-text mt-3">
+								<span className="details-subheading">Location:</span> <br /> {restaurant.address}, {restaurant.city}
+							</Card.Text>
+							<Card.Text className="card-text mt-3">
+								<span className="details-subheading">Phone:</span> <br /> {restaurant.phone || "N/A"}
+							</Card.Text>
+							<Card.Text className="card-text mt-3">
+								<span className="details-subheading">Email:</span> <br /> {restaurant.email || "N/A"}
+							</Card.Text>
+							<div className="card-text mt-3">
+								<span className="details-subheading">Website:</span> <br />{" "}
+								<a href={`https://${restaurant.webpage}`} target="_blank">
+									{restaurant.webpage || "N/A"}
+								</a>
+							</div>
+							<div className="card-text mt-3">
+								<span className="details-subheading">Social Media:</span> <br />
+								<span className="social-media-container d-flex gap-2">
+									{!restaurant.email && !restaurant.facebook && !restaurant.instagram && <p>Data missing</p>}
+									{restaurant.facebook !== "" && (
+										<a href={restaurant.facebook} className="fa-icon">
+											<FaFacebook />
+										</a>
+									)}
+									{restaurant.instagram !== "" && (
+										<a href={restaurant.instagram} className="fa-icon">
+											<FaInstagram />
+										</a>
+									)}
+								</span>
+							</div>
+						</Card.Body>
+					</Card>
+				</div>
+			</Container>
+		</>
+	)
 }
 
 export default RestaurantDetails
